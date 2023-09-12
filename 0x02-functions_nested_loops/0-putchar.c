6c3fd7700143cf8facba20fca8e853e42380edf6
edwkpy <ekipyeckon@gmail.com>
2023-09-12 04:58:35 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-printf("_putchar\n");
-return (0);
-}
+#include "main.h"
+        /**
+         * main - print _putchar
+         * is written by mbah
+         * Return: 0
+         */
+        int main(void)
+        {
+        putchar('');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
+        }
