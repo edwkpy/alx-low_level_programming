27a5f11b98674ea99cb1b2c212bfb0fc5002ff81
edwkpy <ekipyeckon@gmail.com>
2023-09-12 06:34:05 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,25 @@
-include "main.h"
+#include "main.h"
+
 /**
- * print_alphabet_x10 - Print lowercase alphabet 10 times
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
-        char num, letter;
-        for (num = 0; num <= 9; num++)
-        {
-                for (letter = 'a'; letter <= 'z'; letter++)
-                {
-                        _putchar(letter);
-                }
-                _putchar('\n');
-        }
-}
+	char ch;
+	int i;
 
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
