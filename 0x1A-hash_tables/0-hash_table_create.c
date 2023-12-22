c4a9b61681025c5f959d9d3872e48d4bcb293f4c
edwkpy <141271186+edwkpy@users.noreply.github.com>
2023-12-22 20:09:13 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,33 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - FUnction that creates a hash table
- * @size: size of array
- * Return: pointer to newly created hash table
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	new_table = malloc(sizeof(hash_table_t));
-
-	if (new_table == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (new_table->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-	i = 0;
-	while (i < size)
-	{
-		new_table->array[i] = NULL;
-		i++;
-	}
-
-	return (new_table);
+	return (ht);
 }
